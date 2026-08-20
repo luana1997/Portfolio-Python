5fe7c48b7b54895a946ff58b50b9a2fcc9f1dcf9
Luana S. Santos <88246075+luana1997@users.noreply.github.com>
2026-08-20 19:10:28 -0300

    Add files via upload
    Desafios propostos da aula!

---
+++ 01-PrimeirosComandosPython/Imagens/Desafio02.py
@@ -0,0 +1,5 @@
+Dia = input ('Qual o dia do seu nascimento?')
+Mes = input ('Qual o mês do seu nascimento?')
+Ano = input ('Qual o ano do seu nascimento?')
+print ('Você nasceu no',Dia,'de', Mes,'de',Ano,'.Correto?')
+
---
+++ 01-PrimeirosComandosPython/Imagens/Desafio03.py
@@ -0,0 +1,6 @@
+N1 = int (input ('Digite um número inteiro:'))
+N2 = int (input ('Digite um número inteiro:'))
+
+soma= N1+N2
+
+print ('A soma é de:',soma)
