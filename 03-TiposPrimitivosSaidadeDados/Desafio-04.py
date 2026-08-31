594612dd4a770484b8f0d3e4fad187c9f17e11a8
Luana S. Santos <88246075+luana1997@users.noreply.github.com>
2026-08-31 19:15:26 -0300

    Add files via upload

---
+++ 03-TiposPrimitivosSaidadeDados/Desafio-04.py
@@ -0,0 +1,9 @@
+N = input('Digite algo: ')
+print(type(N)) #O tipo primitivo desse valor é?
+print(N.isspace()) #Só tem espaços?
+print(N.isnumeric()) #É um número?'
+print(N.isalpha()) #É alfabético?
+print(N.isalnum()) #É alfanumérico?
+print(N.isupper()) #Está em maiúsculas?
+print (N.islower()) #Está em minúsculas?
+print(N.istitle()) #Está capitalizado?
