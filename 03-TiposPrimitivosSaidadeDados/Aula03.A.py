48c3fbb25d0460105399cee7b98af529b9a26098
Luana S. Santos <88246075+luana1997@users.noreply.github.com>
2026-08-31 19:12:42 -0300

    Add files via upload

---
+++ 03-TiposPrimitivosSaidadeDados/Aula03.A.py
@@ -0,0 +1,9 @@
+
+N1 = int (input ('Digite um número:'))
+N2 = int (input ('Digite um número:'))
+Soma = N1 + N2
+#print ('A soma entre',N1,"e",N2,'é de', Soma)
+print ('A soma entre {} e {} é de {}:'.format(N1, N2, Soma))
+
+
+
---
+++ 03-TiposPrimitivosSaidadeDados/Aula03.B.py
@@ -0,0 +1,21 @@
+#n = float (input ('Digite um número:'))
+#print(n)
+
+#n = str (input ('Digite um número:'))
+#print(type(n))
+
+# = bool (input ('Digite um número:'))
+#print(n)
+
+#n = input ('Digite um número:')
+#print(n.isnumeric())
+
+#n = input ('Digite um número:')
+#print(n.isalpha())
+
+#n = input ('Digite um número:')
+#print(n.isalnum())
+
+n = input ('Digite um número:')
+print(n.isupper())
+
